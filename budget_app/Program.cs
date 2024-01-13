718b572401b9af7e673f92d992fcf412ff3f9ad6
Grant Deker <117955357+Kiddeke@users.noreply.github.com>
2024-01-12 21:32:25 -0500

    Revert "Add first variable"
    This reverts commit 540bb61323020edef8205ca763148fb749791fe2.

--- budget_app/Program.cs
+++ budget_app/Program.cs
@@ -1,12 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System.Numerics;
-
-Console.WriteLine("Monthly Budget");
-double directDepositAmt = 2800 * 2;
-Console.WriteLine($"Direct Deposit: { directDepositAmt}");
-double mortgage = 2400;
-Console.WriteLine($"Mortgage Amt: { mortgage}");
-double monthlyInvesting = 800;
-Console.WriteLine($"Investing Amt: { monthlyInvesting}");
-double moneyLeft = directDepositAmt - mortgage - monthlyInvesting;
-Console.WriteLine($"Leftover: { moneyLeft}");
+Console.WriteLine("Hello, World!");
